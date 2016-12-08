13a11b848a091835168e2914b6ccd7853a718636
Timur Kashapov <graphikov@gmail.com>
2016-12-08 04:30:21 +0300

    Initial commit

---
+++ src/main/java/com/timurkashapov/jwl/Index.java
@@ -0,0 +1,16 @@
+package com.timurkashapov.jwl;
+
+/**
+ * Entry point for application.
+ * @author Timur Kashapov
+ */
+public class Index {
+
+    /**
+     * Main entry point for application.
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
