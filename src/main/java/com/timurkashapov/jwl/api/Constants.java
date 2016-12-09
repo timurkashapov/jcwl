f637011a7b677df5f7c58d96b3b9de7e6bd7f8e1
Timur Kashapov <graphikov@gmail.com>
2016-12-09 03:20:13 +0300

    refactor javadoc of interface fields.

--- src/main/java/com/timurkashapov/jwl/api/Constants.java
+++ src/main/java/com/timurkashapov/jwl/api/Constants.java
@@ -3,69 +3,83 @@ package com.timurkashapov.jwl.api;
 /**
  * Constants interface.
  * @author Timur Kashapov
- * @since 07.12.2016
+ * @since 0.1
+ * @version 0.1
  */
 public interface Constants {
 
     /*
     * Constants
     */
+
     /**
-    * .
+    * Integer Zero.
     */
     int ZERO = 0;
+
     /**
-    * .
+    * Integer One.
     */
     int ONE = 1;
+
     /**
-    * .
+    * Integer Two.
     */
     int TWO = 2;
+
     /**
-    * .
+    * Integer Five.
     */
     int FIVE = 5;
+
     /**
-    * .
+    * Integer Ten.
     */
     int TEN = 10;
+
     /**
-    * .
+     * Integer Sixty
+     */
+    int SIXTY = 60;
+
+    /**
+    * Integer Hundred.
     */
     int HUNDRED = 100;
+
     /**
-    * .
+    * Integer Two Hundred.
     */
     int TWO_HUNDRED = 200;
+
     /**
-    * .
+    * Integer Five Hundred.
     */
     int FIVE_HUNDRED = 500;
+
     /**
-    * .
+    * Integer Thousand.
     */
     int THOUSAND = 1_000;
+
     /**
-    * .
+    * Integer Ten Thousand.
     */
     int TEN_THOUSAND = 10_000;
+
     /**
-    * .
+    * True.
     */
     boolean TRUE = true;
+
     /**
-    * .
+    * False.
     */
     boolean FALSE = false;
+
     /**
-    * .
+    * Null.
     */
     Object NULL = null;
 
-
-    /**
-    * Tools.
-    */
-    void tool();
 }
