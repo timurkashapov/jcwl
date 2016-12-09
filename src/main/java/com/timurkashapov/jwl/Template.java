ad2071c640ef499a04c51c2d6042c78a8b891ef3
Timur Kashapov <graphikov@gmail.com>
2016-12-09 04:21:57 +0300

    corrected Template class

--- src/main/java/com/timurkashapov/jwl/Template.java
+++ src/main/java/com/timurkashapov/jwl/Template.java
@@ -10,42 +10,6 @@ public class Template {
     /*
      * Constants
      */
-    /**
-    *
-    */
-    public static final int ZERO = 0;
-    /**
-    *
-    */
-    public static final int ONE = 1;
-    /**
-    *
-    */
-    public static final int TWO = 2;
-    /**
-    *
-    */
-    public static final int FIVE = 5;
-    /**
-    *
-    */
-    public static final int TEN = 10;
-    /**
-    *
-    */
-    public static final int HUNDRED = 100;
-    /**
-    *
-    */
-    public static final int THOUSAND = 1_000;
-    /**
-    *
-    */
-    public static final int TEN_THOUSAND = 10_000;
-    /**
-    *
-    */
-    public static final Object NULL = null;
 
     /*
      * Fields
