4ba994d84161d1c03d8a61fc32cf8f2159cd290e
Timur Kashapov <graphikov@gmail.com>
2016-12-17 16:46:33 +0300

    test the get() set() insert() remove() and toString() for Array class

--- src/main/java/com/timurkashapov/jwl/array/Array.java
+++ src/main/java/com/timurkashapov/jwl/array/Array.java
@@ -20,8 +20,7 @@ public final class Array {
      * Empty constructor.
      */
     public Array() {
-        this.length = 0;
-        this.source = new int[this.length];
+
     }
 
     /**
@@ -33,21 +32,75 @@ public final class Array {
     }
 
     /**
-     * Insert the member to the end of the array.
-     * @param member member
+     * Set the element to index place.
+     * @param index index
+     * @param element member
+     */
+    public void set(final int index, final int element) {
+        this.source[index] = element;
+    }
+
+    /**
+     * Get the element from array.
+     */
+    public int get(final int index) {
+        return this.source[index];
+    }
+
+    /**
+     * Insert the element to the end of the array.
+     * @param element member
      * @return extended array
      */
-    public int[] insert(final int member) {
+    public int[] insert(final int element) {
 
-        int[] extended = new int[this.source.length + 1];
+        int[] extended = new int[this.length + 1];
 
-        for (int i = 0; i < this.source.length ; i++) {
+        for (int i = 0; i < this.length ; i++) {
             this.source[i] = i;
             extended[i] = this.source[i];
         }
-        extended[extended.length - 1] = member;
+
+        extended[extended.length - 1] = element;
         this.length = extended.length;
 
         return extended;
+    }
+
+    /**
+     * Remove the element from array.
+     */
+    public void remove(final int element) {
+
+        int i;
+
+        for (i = 0; i < this.length; i++) {
+            if (this.source[i] == element) {
+                System.out.printf("\nFind %d", this.source[i]);
+                break;
+            }
+        }
+
+        this.length--;
+
+        for (int j = i; j < this.length; j++) {
+            this.source[j] = this.source[j + 1];
+        }
+    }
+
+    /**
+     * Print the array.
+     * @return array
+     */
+    @Override
+    public String toString() {
+
+        String s = "";
+
+        for (int i = 0; i < this.length; i++) {
+            s = new StringBuilder(s).append(this.source[i]).append(" ").toString();
+        }
+
+        return s;
     }
 }
--- src/test/java/com/timurkashapov/jwl/array/ArrayTest.java
+++ src/test/java/com/timurkashapov/jwl/array/ArrayTest.java
@@ -20,12 +20,47 @@ public class ArrayTest {
     }
 
     @Test
+    public void testSet() {
+
+        Array array = new Array(5);
+
+        array.set(0, 101);
+        array.set(1, 1341);
+        array.set(2, 1);
+        array.set(3, 10);
+        array.set(4, 1566);
+    }
+
+    @Test
     public void testGet() {
-        //TO-DO
+
+        Array array = new Array(5);
+
+        array.get(0);
+        array.get(1);
+        array.get(2);
+        array.get(3);
+        array.get(4);
+
     }
 
     @Test
     public void testRemove() {
-        // TO-DO
+
+        Array array = new Array(6);
+
+        array.set(0, 1);
+        array.set(1, 541);
+        array.set(2, 1);
+        array.set(3, 10);
+        array.set(4, 196);
+        array.set(5, 69);
+
+        System.out.printf("\n%s",array.toString());
+
+        array.remove(10);
+
+        System.out.printf("\n%s", array.toString());
+
     }
 }
