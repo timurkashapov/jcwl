77c66d8e83e3ed90c10c9a8dc31aac7466f6144b
Timur Kashapov <graphikov@gmail.com>
2016-12-17 23:24:56 +0300

    add Stopwatch test

---
+++ src/test/java/com/timurkashapov/jwl/time/StopwatchTest.java
@@ -0,0 +1,19 @@
+package com.timurkashapov.jwl.time;
+
+import org.junit.Test;
+
+/**
+ * Stopwatch testing.
+ * @author Timur Kashapov
+ */
+public final class StopwatchTest {
+
+    /**
+     * Testing the start().
+     * @throws Exception exception.
+     */
+    @Test
+    public void start() throws Exception {
+        // TO-DO
+    }
+}
