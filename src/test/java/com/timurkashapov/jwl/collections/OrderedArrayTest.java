1ff39004f575539a3713ee157fd8eacd1deaaa62
Timur Kashapov <graphikov@gmail.com>
2016-12-21 02:50:59 +0300

    testing

--- src/test/java/com/timurkashapov/jwl/collections/OrderedArrayTest.java
+++ src/test/java/com/timurkashapov/jwl/collections/OrderedArrayTest.java
@@ -1,5 +1,6 @@
 package com.timurkashapov.jwl.collections;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,6 +9,14 @@ import static org.junit.Assert.*;
  * @author Timur Kashapov
  */
 public class OrderedArrayTest {
+
+    OrderedArray orderedArray;
+
+    @Before
+    public void createInstance() {
+        orderedArray = new OrderedArray(new int[]{10, 52, -2, -0, -33333, -9989, 1});
+    }
+
     @Test
     public void getArray() throws Exception {
 
