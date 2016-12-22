2e13f2de8d6103f1ab10290b0ba80e0242217a30
Timur Kashapov <graphikov@gmail.com>
2016-12-22 21:49:11 +0300

    Comparator pre-test realization

---
+++ src/main/java/com/timurkashapov/jwl/algorithms/Comparator.java
@@ -0,0 +1,36 @@
+package com.timurkashapov.jwl.algorithms;
+
+/**
+ * The comparator.
+ *
+ * @author Timur Kashapov
+ */
+public class Comparator {
+
+    /**
+     * constructor.
+     */
+    public Comparator() {
+        // TO-DO
+    }
+
+    /**
+     * Compare the two char's.
+     *
+     * @param first  first char.
+     * @param second second char.
+     * @return result of compare.
+     */
+    public static boolean compare(char first, char second) {
+
+        boolean result = false;
+
+        int f = first;
+        int s = second;
+
+        System.out.printf("\nFirst: %d Second: %d", f, s);
+
+        return result;
+    }
+
+}
